ba224357465bdf0d30e60eb242ab33b829ac7160
fosforizo <163764401+fosforizo@users.noreply.github.com>
2025-05-03 15:37:02 +0300

    Delete cpp-and-qt-workshop-main/cpp-and-qt-workshop-main directory

--- cpp-and-qt-workshop-main/cpp-and-qt-workshop-main/lesson_1/code_examples/hello_world_with_lib/src/hello_lib/hello_world.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string_view>
-#include <hello_lib_export.h>
-
-namespace UniMacedonia {
-    HELLO_LIB_EXPORT std::string_view hello_world();
-}
--- cpp-and-qt-workshop-main/cpp-and-qt-workshop-main/lesson_3/calculator/src/parser/parser.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <ast.h>
-
-// Parse a string and returns it's math expression.
-Expression parse(std::string s);
